2c91a4ad47c2d8e2079e6bcbbb1fe25dadd6c51a
Mohammad Khair Sultan <sultanmohammad94@gmail.com>
2024-01-13 20:05:35 +0300

    built in-memory user repository

---
+++ app/repositories/user_repositories.py
@@ -0,0 +1,56 @@
+from abc import ABC, abstractmethod
+from typing import List, Optional
+from models.user import UserBaseModel
+
+class IUserRepository(ABC):
+    """This Interface represents the base interface for the user repositories."""
+    
+    @abstractmethod
+    def create_user(self, user: UserBaseModel) -> UserBaseModel:
+        '''Add a user to the repository.'''
+        pass
+
+    @abstractmethod
+    def get_user(self, user_id: str) -> Optional[UserBaseModel]:
+        '''Get a user from the repository using its UUID'''
+        pass
+
+    @abstractmethod
+    def update_user(self, user_uuid: str, user: UserBaseModel) -> Optional[UserBaseModel]:
+        '''Update a user from the repository'''
+        pass
+
+    @abstractmethod
+    def delete_user(self, user_id: str) -> bool:
+        '''Delete a user from the repository'''
+        pass
+
+
+class UserInMemoryRepository(IUserRepository):
+    """This class represents a user in memory repository."""
+    def __init__(self):
+        self.users = {}
+
+    def create_user(self, user: UserBaseModel) -> UserBaseModel:
+        '''Add a user to the repository'''
+        if user.UUID in self.users:
+            raise ValueError('User already exists')
+        
+        self.users[user.UUID] = user
+        return user
+
+    def get_user(self, user_uuid: str) -> Optional[UserBaseModel]:
+        '''Get a user from the repository using uuid''' 
+        return self.users.get(user_uuid)
+
+    def update_user(self, user_id: str, user: UserBaseModel) -> Optional[UserBaseModel]:
+        if user_id in self.users:
+            self.users[user_id] = user
+            return user
+        return None
+
+    def delete_user(self, user_id: str) -> bool:
+        if user_id in self.users:
+            del self.users[user_id]
+            return True
+        return False
---
+++ tests/repositories/test_repository_user.py
@@ -0,0 +1,73 @@
+import unittest
+from repositories.user_repositories import UserInMemoryRepository
+from factories.user_factory import UserFactory
+from models.user import UserBaseModel
+from utils.logger import ApplicationLogger
+
+
+class TestUserInMemoryRepository(unittest.TestCase):
+    def setUp(self):
+        self.user_repository = UserInMemoryRepository()
+        self.user_data = UserFactory.create()
+        
+        
+    def test_create_user(self):
+        user = UserBaseModel(**self.user_data.model_dump())
+        user.first_name = 'John'
+        created_user = self.user_repository.create_user(user)
+        self.assertEqual(created_user.first_name, "John")
+
+    def test_get_user(self):
+        # Creating some test users
+        users = UserFactory.create_batch(5)
+        
+        # Adding test users to the repository
+        for user in users:
+            test_user = user
+            self.user_repository.create_user(test_user)
+        
+        # Add our test user to the repository
+        self.user_repository.create_user(self.user_data)
+
+        # Trying to fetch the test user
+        user = self.user_repository.get_user(self.user_data.UUID)
+        self.assertIsNotNone(user)
+        self.assertEqual(user.first_name, self.user_data.first_name)
+
+    def test_update_user(self):
+        
+        # Adding the test user to the repository
+        self.user_repository.create_user(self.user_data)
+        
+        # Getting the id of the test user
+        user_UUID = self.user_data.UUID
+        # set new data for the test user
+        updated_user_data = {
+            "first_name": "Updated",
+            "last_name": "Doe",
+            "email": "updated.doe@example.com",
+            "UUID": user_UUID
+        }
+        # creating a new user with the updated data
+        updated_user = UserBaseModel(**updated_user_data)
+        
+        # Trying to update the user
+        result = self.user_repository.update_user(user_UUID, updated_user)
+        self.assertIsNotNone(result)
+
+    def test_delete_non_existing_user(self):
+        # Try to delete non-existing user
+        user_UUID = self.user_data.UUID
+        result = self.user_repository.delete_user(user_UUID)
+        self.assertFalse(result)
+        
+    def test_delete_existing_user(self):
+        # Adding the user to the repository
+        self.user_repository.create_user(self.user_data)
+        user_UUID = self.user_data.UUID
+        result = self.user_repository.delete_user(user_UUID)
+        self.assertTrue(result)
+        
+
+if __name__ == '__main__':
+    unittest.main()
